822741834fb03498bb400009ce941ddc20320eff
alex <alex.smartens@gmail.com>
2020-08-03 13:44:55 -0400

    Implemented redis subscribtion service

--- game/modules.py
+++ game/modules.py
@@ -1,5 +1,7 @@
 import random
 import string
+import json
+import eventlet
 
 
 def get_random_code():
@@ -8,3 +10,64 @@ def get_random_code():
     """
     letters = string.ascii_lowercase
     return ''.join(random.choice(letters) if i != 4 else '-' for i in range(9))
+
+
+class RedisSubscriptionService:
+    """
+    A thread-like  object, that subscribes to all messages in redis and informs clients in the specified rooms and
+    maintains its subscription in the background (when started).
+    """
+
+    def __init__(self, redis_client, channel_name, socketio, logger):
+        self.pubsub = redis_client.pubsub()
+        self.pubsub.subscribe(channel_name)
+        self.socketio = socketio
+        self.logger = logger
+
+    def __iter_data(self):
+        """
+        Returns redis posts of data type "message" when they are published.
+
+        Returns:
+            msg_str - (str) a JSON with the message content.
+        """
+        for post in self.pubsub.listen():
+            if post["type"] == "message":
+                msg_str = post.get("data")
+                yield msg_str
+
+    def send(self, msg_str):
+        """
+        Sends out the message.
+
+        Arguments:
+            msg_str - (str) a JSON with the message content. This string should be a JSON string and contain at least
+                two keys "room_name" (intended destination), and "type" (message type)
+
+        """
+        try:
+            msg = json.loads(msg_str)
+        except json.decoder.JSONDecodeError:
+            self.logger.error(f"Incorrect message format was read: {msg_str}. A correct message should be a "
+                              f"JSON string")
+        else:
+            if "type" in msg and "room_name" in msg:
+                room_name = msg["room_name"]
+                del msg["room_name"]
+                self.socketio.send(msg, room=room_name)
+            else:
+                self.logger.warning(f"Incorrect message format was read: {msg_str}. A correct message should have "
+                                    f"'room_name' and 'type' keys")
+
+    def run(self):
+        """
+        Listens for new messages and sends them to the specified rooms
+        """
+        for msg_str in self.__iter_data():
+            eventlet.spawn(self.send, msg_str)
+
+    def start(self):
+        """
+        Maintains Redis subscription in the background.
+        """
+        eventlet.spawn(self.run)
--- game_server.py
+++ game_server.py
@@ -5,7 +5,7 @@ import redis
 from flask import Flask, render_template, request
 from flask_socketio import SocketIO, join_room
 
-from game.modules import get_random_code
+from game.modules import get_random_code, RedisSubscriptionService
 
 
 # Initialize the app
@@ -17,21 +17,18 @@ gunicorn_logger = logging.getLogger("gunicorn.error")
 app.logger.handlers = gunicorn_logger.handlers
 app.logger.setLevel(gunicorn_logger.level)
 # Configure redis
+REDIS_CHANNEL_NAME = "hq_trivia"
 REDIS_URL = os.environ.get("REDIS_URL")
 redis_client = redis.from_url(REDIS_URL, decode_responses=True)
-redis_pubsub = redis.pubsub()
 
 # Configure the game server
 SERVER_INSTANCE_NAME = "SERVER" + get_random_code()
 MIN_PLAYERS = 2  # Minimum number of players to start a game
 
 
-@app.route("/")
-def load_web_page():
-    """
-    Returns the main html page.
-    """
-    return render_template("index.html")
+# Run in the background
+redis_subscription = RedisSubscriptionService(redis_client, REDIS_CHANNEL_NAME, socketio, app.logger)
+redis_subscription.start()
 
 
 if __name__ == "__main__":
