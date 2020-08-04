1b6d27be9495e0df49d032696160e0220af870b9
alex <alex.smartens@gmail.com>
2020-08-03 20:53:23 -0400

    Implemented methods for dealing with questions

--- game/questionnaire.py
+++ game/questionnaire.py
@@ -2,8 +2,10 @@
 
 import json
 import random
+from collections import deque
 
 import game.config_variables as conf
+from game.modules import get_random_set
 
 
 def load_questions2redis(redis_client, file_path=None, file_ext=None, category_dict=None):
@@ -53,7 +55,58 @@ def load_questions2redis(redis_client, file_path=None, file_ext=None, category_d
                    ("answer" in question) and \
                    ("alternateSpellings" in question) and \
                    ("suggestions" in question), \
-                "One or more keys are missing the selected JSON string question, the expected keys are 'category', " \
-                "'question', 'answer', 'alternateSpellings', 'suggestions'"
+                f"One or more keys are missing the selected JSON string question, the expected keys are 'category', " \
+                f"'question', 'answer', 'alternateSpellings', 'suggestions'. The encountered question: {question}"
+            question["question"] = question["question"].replace("<BLANK>", "_______")
             redis_client.hset(redis_key, idx, json.dumps(question))
     del questions
+
+
+def map_question_str2dict(question_str):
+    """
+    Maps a JSON string to a dictionary which is ready to be played.
+
+     Arguments:
+         question_str - (str) question in the JSON string format (includes the following keys:
+            'category', 'question', 'answer', 'alternateSpellings', 'suggestions').
+
+     Returns:
+         question - (dict) a question dictionary ready to by played, which includes a "question", a correct "answer" and
+            3 "options" including the correct option.
+     """
+    question_raw = json.loads(question_str)
+    question = {}
+    question["question"] = question_raw["question"]
+    question["answer"] = question_raw["answer"] if len(question_raw["alternateSpellings"]) == 0 or random.randint(0, 1) == 0\
+        else random.choice(question_raw["alternateSpellings"])
+    question["options"] = random.sample(question_raw["suggestions"], 2) + [question["answer"]]
+    random.shuffle(question["options"])
+    return question
+
+
+def get_random_questions(redis_client, difficulty_level, q_len):
+    """
+     Gets a list of random questions of the specified difficulty from redis.
+
+     Note:
+         This function treats redis questions as they all are in the same JSON string format as the ones in "questions_1.json".
+
+     Arguments:
+         redis_client - (obj) redis client, where the questions are to be read.
+         difficulty_level - (str) the redis key to the questions with the specified difficulty.
+         q_len - (int) number of questions to be returned.
+
+     Returns:
+         indices - (set) set of the question indices (for assuring that the game does not run the same question twice,
+            if additional questions are polled during the game).
+         question_q - (deque) queue of question. Each question is stored in a dictionary format, with the following
+            keys: "question", "options", "answer".
+
+     """
+    num_questions = redis_client.hlen(difficulty_level)
+    indices = get_random_set(q_len, 0, num_questions)
+    questions_json = redis_client.hmget(difficulty_level, *indices)
+    question_q = deque()
+    for question_json in questions_json:
+        question_q.append(map_question_str2dict(question_json))
+    return indices, question_q
